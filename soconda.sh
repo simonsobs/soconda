9abf5460ea2deb6a25800660ef3d4c401efbbf0a
Theodore Kisner <tskisner.public@gmail.com>
2023-08-09 10:49:55 -0700

    Work in progress on lmod files

--- soconda.sh
+++ soconda.sh
@@ -74,88 +74,87 @@ envroot=$(basename ${envname})
 # The full environment name, including the root and version.
 fullenv="${envname}-${version}"
 
-# # Activate the base environment
+# Activate the base environment
 
-# if [ "x$(which conda)" = "x" ]; then
-#     # conda is not in the path
-#     if [ "x${base}" = "x" ]; then
-#         # User did not specify where to find it
-#         echo "You must either activate the conda base environment before"
-#         echo "running this script, or you must specify the path to the base"
-#         echo "install with the \"-b <path to base>\" option."
-#         exit 1
-#     fi
-#     conda_dir="${base}"
-# else
-#     # Conda is in the path
-#     conda_dir="$(dirname $(dirname $(which conda)))"
-# fi
-# # Make sure that conda is initialized
-# source "${conda_dir}/etc/profile.d/conda.sh"
-# conda activate base
+if [ "x$(which conda)" = "x" ]; then
+    # conda is not in the path
+    if [ "x${base}" = "x" ]; then
+        # User did not specify where to find it
+        echo "You must either activate the conda base environment before"
+        echo "running this script, or you must specify the path to the base"
+        echo "install with the \"-b <path to base>\" option."
+        exit 1
+    fi
+    conda_dir="${base}"
+else
+    # Conda is in the path
+    conda_dir="$(dirname $(dirname $(which conda)))"
+fi
 
-# # Determine whether the new environment is a name or a full path.
-# env_noslash=$(echo "${fullenv}" | sed -e 's/\///g')
-# is_path=no
-# if [ "${env_noslash}" != "${fullenv}" ]; then
-#     # This was a path
-#     is_path=yes
-#     env_check=""
-#     if [ -e "${fullenv}/bin/conda" ]; then
-# 	    # It already exists
-# 	    env_check="${fullenv}"
-#     fi
-# else
-#     env_check=$(conda env list | grep "${fullenv} ")
-# fi
+# Make sure that conda is initialized
+source "${conda_dir}/etc/profile.d/conda.sh"
+conda activate base
 
-# if [ "x${env_check}" = "x" ]; then
-#     # Environment does not yet exist.  Create it.
-#     if [ ${is_path} = "no" ]; then
-# 	    echo "Creating new environment \"${fullenv}\""
-# 	    conda create --yes -n "${fullenv}"
-#     else
-# 	    echo "Creating new environment \"${fullenv}\""
-# 	    conda create --yes -p "${fullenv}"
-#     fi
-#     echo "Activating environment \"${fullenv}\""
-#     conda activate "${fullenv}"
-#     echo "Setting default channel in this env to conda-forge"
-#     conda config --env --add channels conda-forge
-#     conda config --env --set channel_priority strict
-# else
-#     echo "Activating environment \"${fullenv}\""
-#     conda activate "${fullenv}"
-#     conda env list
-# fi
+# Determine whether the new environment is a name or a full path.
+env_noslash=$(echo "${fullenv}" | sed -e 's/\///g')
+is_path=no
+if [ "${env_noslash}" != "${fullenv}" ]; then
+    # This was a path
+    is_path=yes
+    env_check=""
+    if [ -e "${fullenv}/bin/conda" ]; then
+        # It already exists
+        env_check="${fullenv}"
+    fi
+else
+    env_check=$(conda env list | grep "${fullenv} ")
+fi
 
-# # Install conda packages
+if [ "x${env_check}" = "x" ]; then
+    # Environment does not yet exist.  Create it.
+    if [ ${is_path} = "no" ]; then
+        echo "Creating new environment \"${fullenv}\""
+        conda create --yes -n "${fullenv}"
+    else
+        echo "Creating new environment \"${fullenv}\""
+        conda create --yes -p "${fullenv}"
+    fi
+    echo "Activating environment \"${fullenv}\""
+    conda activate "${fullenv}"
+    echo "Setting default channel in this env to conda-forge"
+    conda config --env --add channels conda-forge
+    conda config --env --set channel_priority strict
+else
+    echo "Activating environment \"${fullenv}\""
+    conda activate "${fullenv}"
+    conda env list
+fi
 
-# echo "Installing conda packages..." | tee "log_conda"
-# conda install --yes --update-all --file "${scriptdir}/packages_conda.txt" \
-#     2>&1 | tee -a "log_conda"
-# # The "cc" symlink from the compilers package shadows Cray's MPI C compiler...
-# rm -f "${CONDA_PREFIX}/bin/cc"
+# Install conda packages
 
-# echo "After install, before deactivation CC=${CC} CXX=${CXX}"
-# conda deactivate
-# conda activate ${fullenv}
-# echo "After re-activation CC=${CC} CXX=${CXX}"
+echo "Installing conda packages..." | tee "log_conda"
+conda install --yes --update-all --file "${scriptdir}/packages_conda.txt" \
+    2>&1 | tee -a "log_conda"
+# The "cc" symlink from the compilers package shadows Cray's MPI C compiler...
+rm -f "${CONDA_PREFIX}/bin/cc"
 
-# # Install mpi4py
+conda deactivate
+conda activate ${fullenv}
 
-# echo "Installing mpi4py..." | tee "log_mpi4py"
-# if [ "x${MPICC}" = "x" ]; then
-#     echo "The MPICC environment variable is not set.  Installing mpi4py" \
-#         | tee -a "log_mpi4py"
-#     echo "from the conda package, rather than building from source." \
-#         | tee -a "log_mpi4py"
-#     conda install --yes mpich mpi4py 2>&1 | tee -a "log_mpi4py"
-# else
-#     echo "Building mpi4py with MPICC=\"${MPICC}\"" | tee -a "log_mpi4py"
-#     pip install --force-reinstall --no-cache-dir --no-binary=mpi4py mpi4py \
-#         2>&1 | tee -a "log_mpi4py"
-# fi
+# Install mpi4py
+
+echo "Installing mpi4py..." | tee "log_mpi4py"
+if [ "x${MPICC}" = "x" ]; then
+    echo "The MPICC environment variable is not set.  Installing mpi4py" \
+        | tee -a "log_mpi4py"
+    echo "from the conda package, rather than building from source." \
+        | tee -a "log_mpi4py"
+    conda install --yes mpich mpi4py 2>&1 | tee -a "log_mpi4py"
+else
+    echo "Building mpi4py with MPICC=\"${MPICC}\"" | tee -a "log_mpi4py"
+    pip install --force-reinstall --no-cache-dir --no-binary=mpi4py mpi4py \
+        2>&1 | tee -a "log_mpi4py"
+fi
 
 # Install local packages
 
@@ -179,38 +178,46 @@ done < "${scriptdir}/packages_local.txt"
 
 echo "Installing pip packages..." | tee "log_pip"
 
-# while IFS='' read -r line || [[ -n "${line}" ]]; do
-#     # Is this line commented?
-#     comment=$(echo "${line}" | cut -c 1)
-#     if [ "${comment}" != "#" ]; then
-#         pkg="${line}"
-#         echo "Installing package ${pkg}"
-#         pip install --no-deps ${pkg} 2>&1 | tee -a "log_pip"
-#     fi
-# done < "${scriptdir}/packages_pip.txt"
+while IFS='' read -r line || [[ -n "${line}" ]]; do
+    # Is this line commented?
+    comment=$(echo "${line}" | cut -c 1)
+    if [ "${comment}" != "#" ]; then
+        pkg="${line}"
+        echo "Installing package ${pkg}"
+        pip install --no-deps ${pkg} 2>&1 | tee -a "log_pip"
+    fi
+done < "${scriptdir}/packages_pip.txt"
 
 # Create and install module file
 
-# if [ "x${moduledir}" = "x" ]; then
-#     # No centralized directory was specified for modulefiles.  Make
-#     # a subdirectory within the environment itself.
-#     moduledir="${CONDA_PREFIX}/modulefiles"
-# fi
-# mkdir -p "${moduledir}/${envroot}"
-# outmod="${moduledir}/${envroot}/${version}"
-# rm -f "${outmod}"
+if [ "x${moduledir}" = "x" ]; then
+    # No centralized directory was specified for modulefiles.  Make
+    # a subdirectory within the environment itself.
+    moduledir="${CONDA_PREFIX}/modulefiles"
+fi
+mkdir -p "${moduledir}/${envroot}"
+outmod="${moduledir}/${envroot}/${version}"
+rm -f "${outmod}"
 
-# confsub="-e 's#@VERSION@#${version}#g'"
-# confsub="${confsub} -e 's#@BASE@#${conda_dir}#g'"
-# confsub="${confsub} -e 's#@ENVNAME@#${fullenv}#g'"
-# confsub="${confsub} -e 's#@ENVPREFIX@#${CONDA_PREFIX}#g'"
+confsub="-e 's#@VERSION@#${version}#g'"
+confsub="${confsub} -e 's#@BASE@#${conda_dir}#g'"
+confsub="${confsub} -e 's#@ENVNAME@#${fullenv}#g'"
+confsub="${confsub} -e 's#@ENVPREFIX@#${CONDA_PREFIX}#g'"
 
-# while IFS='' read -r line || [[ -n "${line}" ]]; do
-#     if [[ "${line}" =~ @MODLOAD@ ]]; then
-#         if [ -e "${modinit}" ]; then
-#             cat "${modinit}" >> "${outmod}"
-#         fi
-#     else
-#         echo "${line}" | eval sed ${confsub} >> "${outmod}"
-#     fi
-# done < "${scriptdir}/templates/modulefile.in"
+if [ "x${LMOD_VERSION}" = "x" ]; then
+    # Using TCL modules
+    input_mod="${scriptdir}/templates/modulefile_tcl.in"
+else
+    # Using LMOD
+    input_mod="${scriptdir}/templates/modulefile_lua.in"
+fi
+
+while IFS='' read -r line || [[ -n "${line}" ]]; do
+    if [[ "${line}" =~ @MODLOAD@ ]]; then
+        if [ -e "${modinit}" ]; then
+            cat "${modinit}" >> "${outmod}"
+        fi
+    else
+        echo "${line}" | eval sed ${confsub} >> "${outmod}"
+    fi
+done < "${input_mod}"
