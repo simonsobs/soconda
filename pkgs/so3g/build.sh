6585639fb3ae17004641d274912d580c9ae6b9a6
Ted Kisner <tskisner.public@gmail.com>
2024-04-02 14:20:06 -0700

    Bump so3g version, add include directory to cxxflags

--- pkgs/so3g/build.sh
+++ pkgs/so3g/build.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 CFLAGS="-O3 -g -fPIC -I${PREFIX}/include" \
-CXXFLAGS='-O3 -g -fPIC -std=c++14' \
+CXXFLAGS="-O3 -g -fPIC -std=c++14 -I${PREFIX}/include" \
 BOOST_ROOT="${PREFIX}" \
 FLAC_ROOT="${PREFIX}" \
 python -m pip install -vvv --ignore-installed --no-deps --prefix "${PREFIX}" .
