2b95a47531f588c7c038aaff51755290a8657e61
Ted Kisner <tskisner.public@gmail.com>
2024-04-03 20:45:04 -0700

    Additional work on openmp conflicts

--- pkgs/so3g/build.sh
+++ pkgs/so3g/build.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-CFLAGS="-O3 -g -fPIC -I${PREFIX}/include" \
-CXXFLAGS="-O3 -g -fPIC -std=c++14 -I${PREFIX}/include" \
+CFLAGS="-Wno-error -O3 -g -fPIC -I${PREFIX}/include" \
+CXXFLAGS="-Wno-error -O3 -g -fPIC -std=c++14 -I${PREFIX}/include" \
 BOOST_ROOT="${PREFIX}" \
 FLAC_ROOT="${PREFIX}" \
 python -m pip install -vvv --ignore-installed --no-deps --prefix "${PREFIX}" .
