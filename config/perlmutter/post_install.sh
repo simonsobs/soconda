6e2ca2bedfa2708d7c3f0decc9093bcc3edf06df
Ioannis Paraskevakos <iparask@princeton.edu>
2025-10-24 13:45:41 -0400

    fix: temporarily disable from perlmutter

--- config/perlmutter/post_install.sh
+++ config/perlmutter/post_install.sh
@@ -15,7 +15,3 @@ install_jupyter_setup=yes
 # Add any other shell commands here for this system...
 
 # Add radical.pilot SO config in the correct folder.
-python ${script_dir}/tools/update_resource.py \
-       --config ${config} \
-       --base-json ${script_dir}/templates/resource_so.json \
-       --output-json ${CONDA_PREFIX}/lib/python${python_major_minor}/site-packages/radical/pilot/configs/resource_so.json
