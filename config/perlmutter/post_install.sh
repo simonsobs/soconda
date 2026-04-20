c244680026602a63815a4e937df9fe3abca89cb4
Bai-Chiang <contact@bai-chiang.com>
2026-04-20 16:43:47 -0500

    Install sogma (GPU mapmaker) on perlmutter

--- config/perlmutter/post_install.sh
+++ config/perlmutter/post_install.sh
@@ -20,3 +20,50 @@ python ${scriptdir}/tools/update_resource.py \
        --base-json ${scriptdir}/templates/resource_so.json \
        --output-json ${CONDA_PREFIX}/lib/python${python_major_minor}/site-packages/radical/pilot/configs/resource_so.json \
        --env-path ${CONDA_PREFIX}
+
+
+conda_exec env config vars set SOPATH=/global/cfs/cdirs/sobs
+conda_exec env config vars set PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$CONDA_PREFIX/share/pkgconfig:$PKG_CONFIG_PATH
+conda_exec deactivate
+conda_exec activate "${fullenv}"
+
+mkdir -p $CONDA_PREFIX/git
+
+echo ''
+echo ''
+cd $CONDA_PREFIX/git
+git clone https://github.com/amaurea/fast_g3.git
+cd $CONDA_PREFIX/git/fast_g3
+make clean && make -j4 && ln -sf $CONDA_PREFIX/git/fast_g3/fast_g3 $CONDA_PREFIX/lib/python${python_major_minor}/site-packages/fast_g3
+
+echo ''
+echo ''
+cd $CONDA_PREFIX/git
+git clone https://github.com/amaurea/cpu_mm.git
+cd $CONDA_PREFIX/git/cpu_mm
+make clean && make -j4 && ln -sf $CONDA_PREFIX/git/cpu_mm/python $CONDA_PREFIX/lib/python${python_major_minor}/site-packages/cpu_mm
+
+echo ''
+echo ''
+cd $CONDA_PREFIX/git
+git clone https://github.com/amaurea/gpu_mm.git
+cd $CONDA_PREFIX/git/gpu_mm
+make clean && make -j4 && ln -sf $CONDA_PREFIX/git/gpu_mm/gpu_mm $CONDA_PREFIX/lib/python${python_major_minor}/site-packages/gpu_mm
+
+echo ''
+echo ''
+cd $CONDA_PREFIX/git
+git clone https://github.com/amaurea/sogma.git
+cd $CONDA_PREFIX/git/sogma
+ln -sf $CONDA_PREFIX/git/sogma/python $CONDA_PREFIX/lib/python${python_major_minor}/site-packages/sogma
+ln -sf $CONDA_PREFIX/git/sogma/bin/sogma $CONDA_PREFIX/bin/sogma
+ln -sf $CONDA_PREFIX/git/sogma/bin/solist $CONDA_PREFIX/bin/solist
+ln -sf $CONDA_PREFIX/git/sogma/bin/soplanet $CONDA_PREFIX/bin/soplanet
+ln -sf $CONDA_PREFIX/git/sogma/bin/sosplit $CONDA_PREFIX/bin/sosplit
+
+echo ''
+echo ''
+cd $CONDA_PREFIX/git
+git clone https://github.com/amaurea/tenki.git
+ln -sf $CONDA_PREFIX/git/tenki/enplot $CONDA_PREFIX/bin/enplot
+
